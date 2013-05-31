67ccc05a41be22016d0eb79e388f02f9c17fa1e3
svnlabs <svnlabs@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2013-05-31 04:08:30 +0000

    First commit for SC HTML5 Player
    git-svn-id: https://plugins.svn.wordpress.org/html5-soundcloud-player-with-playlist/trunk@720693 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ list.php
@@ -0,0 +1,105 @@
+<?php
+
+global $wpdb;
+$table		=	$wpdb->prefix."html5soundcloud_playlist";
+
+
+$isuccess = isset($_REQUEST['isuccess'])?$_REQUEST['isuccess']:"";
+$ierror = isset($_REQUEST['ierror'])?$_REQUEST['ierror']:"";
+
+$action = isset($_REQUEST['action'])?$_REQUEST['action']:"add";
+$show = isset($_REQUEST['show'])?$_REQUEST['show']:"list";
+
+
+if(isset($_GET['id'])){
+	$id		=	$_GET['id'];
+}
+
+
+$usql		=	"SELECT * FROM $table WHERE id='$id'";
+$uresults 	= 	$wpdb->get_row( $usql  );
+
+
+if($action=="delete") {
+
+$delete		=	$wpdb->query(
+							"DELETE FROM $table WHERE id='$id'"
+						);
+						
+$isuccess	=	"Item deleted successfully";						
+						
+}						
+
+
+?>
+
+
+<h2>HTML5 SoundCloud Player with Playlist Options - <a href="options-general.php?page=html5-soundcloud-player-with-playlist.php" style="background-color:#D84937; padding:5px; height:35px; color:#ffffff; font-weight:bold;">Home</a></h2>
+
+
+
+<?php if(!empty($isuccess)): ?>
+        
+<span style="color:green;"><?php echo $isuccess; ?></span>
+
+<?php elseif(!empty($ierror)): ?>
+
+<span style="color:red;"><?php echo $ierror; ?></span>
+       
+<?php endif ?>
+
+<table class="wp-list-table widefat fixed" cellspacing="0" style="margin-top:20px;">
+	<thead>
+	<tr>		
+        <th scope="col" width="10%"><a href="#">ID</a></th>
+        <th scope="col" width="15%"><a href="#">Description</a></th>
+        <th scope="col" width="10%"><a href="#">Shortcode</a></th>
+        <th scope="col" width="10%"><a href="#">Edit</a></th>	
+        <th scope="col" width="10%"><a href="#">Delete</a></th>	
+     </tr>
+	</thead>
+
+	<tfoot>
+	<tr>
+	    <th scope="col" width="10%"><a href="#">ID</a></th>
+        <th scope="col" width="15%"><a href="#">Description</a></th>
+        <th scope="col" width="10%"><a href="#">Shortcode</a></th>
+        <th scope="col" width="10%"><a href="#">Edit</a></th>	
+        <th scope="col" width="10%"><a href="#">Delete</a></th>		
+     </tr>
+	</tfoot>
+
+	<tbody id="the-list">
+    
+    <?php
+		//$sql		=	"SELECT * FROM $table";
+
+		//$results 	= 	$wpdb->get_results( $wpdb->prepare( $sql ) );
+		
+		$sql		=	mysql_query("SELECT * FROM $table");
+		
+		$mmm = 0;
+		
+	?>
+	
+    <?php while( $result = mysql_fetch_object($sql) ) {   $info = unserialize($result->params);   ?>
+    <tr>
+        <td width="1%"><?php echo $result->id; ?></td>
+        <td width="10%">
+		
+        SoundCloud URL: <strong><?php echo $info['html5soundcloudlink']; ?></strong><br>
+        
+        Player: <?php echo $info['player']; ?><br>
+        
+        </td>
+        <td width="10%">[html5soundcloud id="<?php echo $result->id; ?>"]</td>
+        <td width="10%"><a href="<?php bloginfo('url'); ?>/wp-admin/options-general.php?page=html5-soundcloud-player-with-playlist.php&action=update&id=<?php echo $result->id; ?>">Update</a></td>
+        <td width="10%"><a onclick="return confirm('are you sure?');" href="<?php bloginfo('url'); ?>/wp-admin/options-general.php?page=html5-soundcloud-player-with-playlist.php&show=list&action=delete&id=<?php echo $result->id; ?>">Delete</a></td>
+	</tr>
+	<?php $mmm=1; } ?>
+	
+  	
+  </tbody>
+</table>   
+
+       
---
+++ readme.txt
@@ -0,0 +1,65 @@
+=== HTML5 SoundCloud Player with Playlist ===
+Contributors: Sandeep Verma
+Tags: MP3 Player, SoundCloud Player, HTML5 Player
+Requires at least: 2.0
+Tested up to: 3.5.1
+Stable tag: 1.0.6
+
+
+== Description ==
+
+WordPress and Standalone HTML5 SoundCloud Player with Playlist for SoundCloud Sets compatible with iOS, Android and supports all browsers Firefox, Chrome, Safari, IE and Opera!
+
+**Features**
+
+* Self hosted player files
+* Best support on email
+* Download songs option
+* MP3 Song’s information using ID3 Tags
+* Customize Social Share on Facebook & Twitter
+* Volume Control
+* Embed Code Option
+
+[youtube http://www.youtube.com/watch?feature=player_embedded&v=B_Z9DLx-d14]
+
+
+[HTML5 SoundCloud Player with Playlist](http://html5plus.svnlabs.com/shop/html5-soundcloud-player-with-playlist/)
+
+
+== Installation ==
+
+Unzip "html5-soundcloud-player-with-playlist.zip" and copy folder "html5-soundcloud-player-with-playlist" into your wordpress plugins folder, normally located in /wp-content/plugins/
+
+
+Create new application on SoundCloud http://soundcloud.com/you/apps & get Client Id and Client Secret…
+
+Then you need to enter 
+
+SoundCloud Set URL: ?
+Client ID: ?
+Client Secret: ?
+
+
+Shortcode for Page or Post
+
+[html5soundcloud id="1"]
+
+Or
+
+[html5soundcloud id="" html5soundcloudlink="https://soundcloud.com/svnlabs/sets/svnlabs" clientid="*********************" clientsecret="b155afb0***********616de24eff0" player="vertical" bcolor="cccccc" id3="1" image="http://svnlabs.com/logo.png" title="Folder Player" artist="Sandeep" facebook="http://www.facebook.com/svnlab" twitter="http://twitter.com/svnlabs" shadow="1" autoplay="1" download="1" background="" ]
+
+
+== Frequently Asked Questions ==
+
+http://html5.svnlabs.com/faq/
+
+== Screenshots ==
+
+1. HTML5 SoundCloud Player  
+
+2. HTML5 SoundCloud Player Settings
+
+3. HTML5 SoundCloud Player with saved players
+
+== Changelog ==
+
