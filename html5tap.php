a504cde6e8973f0a30ecb4066f77f7d4217cb161
svnlabs <svnlabs@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2015-01-11 13:23:48 +0000

    title
    git-svn-id: https://plugins.svn.wordpress.org/html5-soundcloud-player-with-playlist/trunk@1065215 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- html5tap.php
+++ html5tap.php
@@ -5,7 +5,7 @@ if ( ! defined( 'ABSPATH' ) ) {
 }
 
 /*
-Plugin Name: HTML5Tap Free
+Plugin Name: HTML5 SoundCloud Player with Playlist
 Plugin URI: http://www.html5tap.com
 Description: HTML5Tap Players enable native audio playback within the browser. It supports all browsers i.e. iOS, Android, Firefox, Chrome, Safari, IE and Opera.
 Version: 2.3
